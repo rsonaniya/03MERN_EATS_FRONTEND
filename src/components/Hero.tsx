9402f54a866d05cd25fb9f68800d298875570381
rsonaniya <rajatsonaniya28@gmail.com>
2024-08-11 14:23:34 +0530

    first commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+import hero from "../assets/hero.png";
+const Hero = () => {
+  return (
+    <div className="">
+      <img src={hero} className="w-full max-h-[500px] object-cover" />
+    </div>
+  );
+};
+
+export default Hero;
